6109f6697808273c2f92f295cf7e5efea2465933
Vinh Tran <vctran62@users.noreply.github.com>
2021-03-29 17:05:25 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,11 @@
 #File =provider.tf
 provider "aws" {
-  access_key          = "${var.access_key}"
-  secret_key          = "${var.secret_key}"
-  region              = "${var.region}"
-  version             = ">= 3.0"
+#  access_key          = "${var.access_key}"
+#  secret_key          = "${var.secret_key}"
+#  region              = "${var.region}"
+#  version             = ">= 3.0"
+  access_key          = var.access_key
+  secret_key          = var.secret_key
+  region              = var.region
+#  version             = ">= 3.0"
 }
